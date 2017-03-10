91fe767535d4581c80332980c02e9492187e0b4c
Mikecruze <162t0307@itsm.edu.mx>
2017-03-09 23:52:14 -0600

    Create CtrOther.java

---
+++ CtrOther.java
@@ -0,0 +1,13 @@
+public class CtrOther {
+public static void main(String [] args){
+	Other o1 = new Other();
+	System.out.println(o1.mensaje()+" x= "+o1.getX()+", y= "+o1.getY());
+	
+	Other o2 = new Other();
+	System.out.println(o2.mensaje()+" x= "+o2.getX()+", y= "+o2.getY());
+	
+	Other o3 = new Other();
+	System.out.println(o3.mensaje()+" x= "+o3.getX()+", y= "+o3.getY());
+    
+}
+}
