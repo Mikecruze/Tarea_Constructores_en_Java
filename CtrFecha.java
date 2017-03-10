7d6321557ac8241aa78481e1fb9e1c55d84a5741
Mikecruze <162t0307@itsm.edu.mx>
2017-03-09 23:15:52 -0600

    Create CtrFecha.java

---
+++ CtrFecha.java
@@ -0,0 +1,13 @@
+public class CtrFecha {
+public static void main(String [] args){
+	Fecha f1 = new Fecha();
+	System.out.println(d1.mensaje()+d1.getDia()+"/"+d1.getMes()+"/"+d1.getAño());
+	
+	Fecha f2 = new Fecha(2016,10,3);
+	System.out.println(d2.mensaje()+d2.getDia()+"/"+d2.getMes()+"/"+d2.getAño());
+	
+	Fecha f3 = new Fecha(1910,9,15);
+	System.out.println(d3.mensaje()+d3.getDia()+"/"+d3.getMes()+"/"+d3.getAño());
+	
+}
+}
